1d122564cc6c3c263f5cdbcd92c568c47e1ee793
BrayanCode11 <bangarita@unal.edu.co>
2023-11-15 23:12:11 -0500

    Refactor CSS and add freeCodeCamp logo and button component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import './App.css';
-
+import "./App.css";
+import Boton from "./componentes/Boton";
+import freCodeCampLogo from "./images/freecodecamp-logo.png";
 function App() {
+
+  const manejarClic = () => {
+    console.log("Se hizo clic");
+  }
+  const reiniciarContador = () => {
+    console.log("Se reinició el contador");
+  }
+
   return (
     <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          className="freecodecamp-logo"
+          src={freCodeCampLogo}
+          alt="Logo de freeCodeCamp"
+        />
+      </div>
+      <div className="contenedor-contador">
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton texto="Reiniciar" esBotonDeClic={false} manejarClic={reiniciarContador} />
+
+      </div>
     </div>
   );
 }
